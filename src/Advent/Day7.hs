cf49a43673a24c1c30933cba5fd27de42b86d258
Simon Fenton <swfenton@gmail.com>
2016-01-04 14:57:14 +1100

    Faffin to get Earley working

--- src/Advent/Day7.hs
+++ src/Advent/Day7.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Advent.Day7 where
 
@@ -11,19 +12,18 @@ type Name = String
 data Val = Val Int deriving (Eq, Show, Ord)
 data Wire = Wire Val Name deriving (Eq, Show, Ord)
 
-wireG :: E.Grammar r (E.Prod r String String Wire) a
+wireG :: Grammar r (Prod r String String Wire)
 wireG = mdo
   -- x1 <- rule $ Add <$> x1 <* namedSymbol "+" <*> x2
   --           <|> x2
   --           <?> "sum"
   -- arrow <- rule $
-  wire <- rule $ Wire <$> val <* namedSymbol "->" <*> (satisfy ident <?> "identifier")
   val <- rule $ Val . read <$> (satisfy num <?> "number")
+  wire <- rule $ Wire <$> val <* symbol "->" <*> (satisfy ident <?> "identifier")
   return wire
   where
     num = all isDigit
     ident = all isAlpha
 
--- parse :: String -> Wire
--- parse :: String -> ([Wire], E.Report Expected String)
--- parse x = E.fullParses (E.parser wireG) $ words x
+parse :: String -> ([Wire], E.Report String String)
+parse x = E.fullParses (E.parser wireG) $ lines x
